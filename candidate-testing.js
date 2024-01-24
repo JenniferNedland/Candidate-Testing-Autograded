8e801586c79192418d2f59d0ea897a2f39a80e94
alacrityj <alacritydanger@gmail.com>
2024-01-24 03:16:38 -0600

    17/17 tests passing

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,62 +1,58 @@
 const input = require('readline-sync');
 
-// TODO: 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
 let candidateName = "";
-
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-
-
-// TODO Variables for Part 2
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
-
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Please enter your name. ");
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  // candidateAnswer = input.question(question);
   for (let i = 0; i < questions.length; i++) {
     candidateAnswers.push(input.question("\n" + [i + 1] + "." + questions[i] + "\n"));
   }
-
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  console.log("\nHere are your results:\n");
+  let grade = 0;
+  let numCorrectAnswers = 0;
+  let isCorrect = false;
+  let didPass = false;
+  
+  console.log("\nQUIZ RESULTS:\n");
+  
   for (let i = 0; i < correctAnswers.length; i++) {
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
-      console.log(`${i + 1}. Correct. You answered ${candidateAnswers[i]}. The correct answer is ${correctAnswers[i]}.\n`);
-    } else {
-      console.log(`${i + 1}. Incorrect. You answered ${candidateAnswers[i]}. The correct answer is ${correctAnswers[i]}.\n`);
+      isCorrect = true;
     }
+    numCorrectAnswers += isCorrect;
+    console.log(`${i + 1}. ${(isCorrect ? ("CORRECT") : ("INCORRECT"))}. You answered ${candidateAnswers[i]}. The correct answer is ${correctAnswers[i]}.\n`);
+    isCorrect = false;
   }
-  let grade;  // TODO: 3.2 use this variable to calculate the candidates score.
-
+  
+  grade = (numCorrectAnswers/questions.length) * 100;
+  console.log(grade);
+    if (grade >= 80) {
+      didPass = true; 
+      console.log(`${candidateName}, you ${(didPass ? ("PASSED") : ("FAILED"))}. You scored ${grade}%. You needed a minimum of 80% to pass.`)
+  }
 
   return grade;
 }
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Greet candidate using their name //
   console.log("Hello, " + candidateName + "!\n");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
 
-// ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
   question: question,
